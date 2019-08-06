53e0776306670436bd61ee3ec84b7156bde16393
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-08-07 00:14:10 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+        int arr[100];
+        int n;
+        printf("Enter the number of terms");
+        scanf("%d",&n);
+        for(int i=0;i<n;i++)
+        {
+                printf("\nEnter a number ");
+                scanf("%d",&arr[i]);
+		}
+		for(int k=0;k<n;k++)
+		{
+			if(k<n-1)
+			{
+				printf("%d",arr[k]);
+			}
+			else
+			{
+				printf("%d",arr[k]);
+			}
+		}
+}
