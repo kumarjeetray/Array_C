be59201568d62195724693bdb5a3df2d42a3856f
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-08-09 08:13:37 +0530

    Add files via upload

---
+++ array10.c
@@ -0,0 +1,23 @@
+void main()
+{
+        int arr[100];
+        int n,n1;
+        printf("Enter the number of terms");
+        scanf("%d",&n);
+        for(int i=0;i<n;i++)
+        {
+            printf("Enter a Number");
+            scanf("%d",&arr[i]);
+        }
+        for(int j=0;j<n;j++)
+        {
+                int swap=arr[j];
+                arr[j]=arr[n-j-1];
+                arr[n-j-1]=swap;
+        }
+        printf("Array After Reversal");
+        for(int k=0;k<n;k++)
+        {
+                printf("%d ,",arr[k]);
+        }
+}
