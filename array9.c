3d0b01bcf5ba683786f9b36e1a862660edcc7dcd
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-08-09 07:58:33 +0530

    Add files via upload

---
+++ array9.c
@@ -0,0 +1,22 @@
+void main()
+{
+        int arr[100];
+        int n,n1;
+        printf("Enter the number of terms");
+        scanf("%d",&n);
+        int arr2[100];
+        for(int i=0;i<n;i++)
+        {
+            printf("Enter a Number");
+            scanf("%d",&arr[i]);
+        }
+        for(int j=0,k=n-1;j<n;j++,k--)
+        {
+            arr[j]=arr2[k];
+        }
+        printf("Elements in Reverse Order:\n");
+        for(int l=0;l<n;l++)
+        {
+                printf("%d",arr2[l]);
+        }
+}
