162bbe54f9fa384bf5876176bbbd143061c1e4d8
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-08-10 22:45:49 +0530

    Add files via upload

---
+++ array14.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+        int arr[100];
+        int n;
+        printf("Enter the No of terms:");
+        scanf("%d",n);
+        for(int i=0;i<n;i++)
+        {
+                printf("\nEnter a Number:");
+                scanf("%d",&arr[i]);
+        }
+        int s,c=0;
+        printf("\nEnter the number you want to search in the array");
+        scanf("%d",&s);
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]==s)
+            {
+                    printf("\nThe number has been found in the array");
+                    c=1;
+            }
+        }
+        if(c==0)
+        {
+                printf("\nThe number cant be found in the array");
+        }
+}
