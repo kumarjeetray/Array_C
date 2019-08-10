c4ad8c29c040608004d6073a58470292255c8988
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-08-10 22:21:29 +0530

    Add files via upload

---
+++ array12.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+        int arr[100];
+        int n;
+        printf("Enter the No of terms:");
+        scanf("%d",n);
+        for(int i=0;i<n;i++)
+        {
+                printf("\nEnter a Number:");
+                scanf("%d",&arr[i]);
+        }
+        for(int j=0;j<n;j++)
+        {
+                for(int k=j+1;k<n;k++)
+                {
+                        if(arr[k]<arr[j])
+                        {
+                            int temp=arr[j];
+                            arr[j]=arr[k];
+                            arr[k]=temp;
+                        }
+                }
+        }
+        printf("\nThe largest number in the array is %d",arr[n-1]);
+        printf("\nThe smallestb number in the array is %d",arr[0]);
+}
